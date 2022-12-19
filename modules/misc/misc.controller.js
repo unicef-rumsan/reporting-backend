b763a50315c1d1a3cffefa94f32c56278295f827
Santosh <santosh@rumsan.com>
2022-12-19 20:25:29 +0545

    cash tracker

--- modules/misc/misc.controller.js
+++ modules/misc/misc.controller.js
@@ -1,7 +1,7 @@
 const config = require("config");
 const { AbstractController } = require("@rumsan/core/abstract");
 const WSService = require("@rumsan/core/services/webSocket");
-const { MiscModel } = require("../models");
+const { MiscModel, BeneficiaryModel, VendorModel } = require("../models");
 const checkToken = require("../../helpers/utils/checkToken");
 
 module.exports = class extends AbstractController {
@@ -18,6 +18,60 @@ module.exports = class extends AbstractController {
 
   async add(name, value) {
     //checkToken(req);
+
+    try {
+      if (name === "cash-tracker-summary") {
+        let totalBenCashBalance = await BeneficiaryModel.findAll({
+          attributes: [
+            [
+              this.db.Sequelize.fn("sum", this.db.Sequelize.col("cashBalance")),
+              "totalBenCashBalance",
+            ],
+          ],
+          raw: true,
+        });
+
+        let totalVendorTokenBalance = await VendorModel.findAll({
+          attributes: [
+            [
+              this.db.Sequelize.fn(
+                "sum",
+                this.db.Sequelize.col("tokenBalance")
+              ),
+              "totalVendorTokenBalance",
+            ],
+          ],
+          raw: true,
+        });
+
+        let totalVendorCashBalance = await VendorModel.findAll({
+          attributes: [
+            [
+              this.db.Sequelize.fn("sum", this.db.Sequelize.col("cashBalance")),
+              "totalVendorCashBalance",
+            ],
+          ],
+          raw: true,
+        });
+
+        totalBenCashBalance = parseInt(
+          totalBenCashBalance[0].totalBenCashBalance
+        );
+        totalVendorTokenBalance = parseInt(
+          totalVendorTokenBalance[0].totalVendorTokenBalance
+        );
+        totalVendorCashBalance = parseInt(
+          totalVendorCashBalance[0].totalVendorCashBalance
+        );
+        let totalVendorCashReceived =
+          totalVendorTokenBalance + totalVendorCashBalance;
+
+        value.wards.received = totalVendorCashReceived;
+        value.wards.disbursed = totalVendorTokenBalance;
+        value.beneficiaries.received = totalBenCashBalance;
+      }
+    } catch (e) {}
+
     let rec = await this.table.findOne({ where: { name } });
     if (rec) {
       rec.set("value", value);
